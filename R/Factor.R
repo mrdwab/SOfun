cc44498cf6c02bef0e53531e38f0216c90af910c
Ananda Mahto <mrdwab@gmail.com>
2014-02-10 22:32:51 +0530

    Rd2roxygen rewraps

--- R/Factor.R
+++ R/Factor.R
@@ -1,31 +1,31 @@
 #' Factor vectors with multiple levels
-#'
-#' \code{\link{factor}} does not let you use duplicated levels nicely. It results
-#' in an ugly warning message and you need to use \code{\link{droplevels}} to get
-#' the desired output.
-#'
-#' The "solution" is to first factor the vector, and then use a named \code{list}
-#' with the \code{\link{levels}} function. This function is a wrapper around
-#' those steps.
-#'
+#' 
+#' \code{\link{factor}} does not let you use duplicated levels nicely. It
+#' results in an ugly warning message and you need to use
+#' \code{\link{droplevels}} to get the desired output.
+#' 
+#' The "solution" is to first factor the vector, and then use a named
+#' \code{list} with the \code{\link{levels}} function. This function is a
+#' wrapper around those steps.
+#' 
 #' @param invec A \code{vector} that needs to be factored.
 #' @param levels A named \code{list} of the levels. The \code{name} is the
 #' level and the values are what should be mapped to those levels.
 #' @param store Logical. Should the input values be stored as an attribute?
 #' @param \dots Additional arguments to \code{factor}.
-#' @return A factored variable with \code{class} of \code{factor} and 
-#' \code{Factor}, optionally with an \code{attribute} of \code{"Input"}
-#' which stores the original input values.
+#' @return A factored variable with \code{class} of \code{factor} and
+#' \code{Factor}, optionally with an \code{attribute} of \code{"Input"} which
+#' stores the original input values.
 #' @author Ananda Mahto
 #' @seealso \code{\link{factor}}, \code{\link{levels}}
 #' @references \url{http://stackoverflow.com/a/19410249/1270695}
 #' @examples
-#'
+#' 
 #' x <- c("Y", "Y", "Yes", "N", "No", "H")
 #' Factor(x, list(Yes = c("Yes", "Y"), No = c("No", "N")))
 #' Factor(x, list(Yes = c("Yes", "Y"), No = c("No", "N")), FALSE)
 #' Factor(x, list(No = c("No", "N"), Yes = c("Yes", "Y")), ordered = TRUE)
-#'
+#' 
 #' @export Factor
 Factor <- function(invec, levels = list(), store = TRUE, ...) {
   Fac <- factor(invec, ...)
