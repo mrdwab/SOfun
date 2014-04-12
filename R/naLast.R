4af6bae27a439df3b57190c9b04a61eff7787d90
Ananda Mahto <mrdwab@gmail.com>
2014-04-12 17:06:05 +0530

    automatic code tidying

--- R/naLast.R
+++ R/naLast.R
@@ -1,18 +1,25 @@
 #' Move all \code{NA} values to the end of the rows or columns of a matrix
 #' 
-#' Moves all of the \code{NA} values in the rows or columns of a matrix to the end of the respective rows or columns.
-#'  
+#' Moves all of the \code{NA} values in the rows or columns of a matrix to the
+#' end of the respective rows or columns.
+#' 
+#' 
 #' @param inmat The input matrix.
-#' @param by Should be either \code{"row"} or \code{"col"}, depending on if you want to shift non-\code{NA} values left or up.
-#' @param outList Logical. Do you just want a \code{list} of the non-\code{NA} values? Defaults to \code{FALSE}.
-#' @param fill While you're at it, do you want to replace \code{NA} with some other value?
-#' @return Either a \code{matrix} with the same dimensions as the input matrix or a \code{list} with the same number of rows or columns as the input matrix (depending on the choice made in \code{by}).
+#' @param by Should be either \code{"row"} or \code{"col"}, depending on if you
+#' want to shift non-\code{NA} values left or up.
+#' @param outList Logical. Do you just want a \code{list} of the non-\code{NA}
+#' values? Defaults to \code{FALSE}.
+#' @param fill While you're at it, do you want to replace \code{NA} with some
+#' other value?
+#' @return Either a \code{matrix} with the same dimensions as the input matrix
+#' or a \code{list} with the same number of rows or columns as the input matrix
+#' (depending on the choice made in \code{by}).
 #' @author Ananda Mahto
 #' @references \url{http://stackoverflow.com/q/23008142/1270695}
 #' @examples
 #' 
 #' set.seed(1)
-#' m <- matrix(sample(25, 20, TRUE), ncol = 4, 
+#' m <- matrix(sample(25, 20, TRUE), ncol = 4,
 #'             dimnames = list(letters[1:5], LETTERS[1:4]))
 #' m[sample(prod(dim(m)), prod(dim(m)) * .6)] <- NA
 #' 
