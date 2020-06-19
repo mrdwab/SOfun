e41fa6220871b68be928dfe57866992181dc4e1d
Ananda Mahto <mrdwab@gmail.com>
2020-06-19 15:10:29 -0700

    changed `Map` to `mapply`

--- R/list_reduction.R
+++ R/list_reduction.R
@@ -4,7 +4,7 @@
 #' at each index level in a `list`.
 #' 
 #' @param inlist The input `list`.
-#' @param fun The function to be applied. Note that the supplied function should be one
+#' @param FUN The function to be applied. Note that the supplied function should be one
 #' that you would expect to have work with two or more `vectors` as the given arguments.
 #' Thus, you can expect `range` to work, but not `mean`.
 #' @param flatten Logical. Should the output be simplified from a `list`. Defaults to
@@ -12,8 +12,11 @@
 #' single value (for example, when using a function like `sum` or `max`).
 #' @param sorted Logical. Should the values at each `list` index be sorted? Defaults
 #' to `FALSE`.
+#' @param MoreArgs A `list` of additional arguments to be passed to `FUN`. See
+#' [base::mapply()] for more details.
 #' @return A `list` (default) or a simple `vector` (if `flatten = TRUE`).
 #' @author Ananda Mahto
+#' @seealso [base::Reduce()], [base::mapply()]
 #' @references See: <https://stackoverflow.com/q/62454705/1270695>
 #' @examples
 #' 
@@ -26,14 +29,14 @@
 #' set.seed(1)
 #' L2 <- replicate(3, replicate(3, sample(sample(20), sample(10), TRUE), FALSE), FALSE)
 #' list_reduction(L2)
-#' list_reduction(L2, sum)
+#' list_reduction(L2, sum, flatten = TRUE)
 #' list_reduction(L2, range)
 #' list_reduction(L2, union)
 #' list_reduction(L2, union, sorted = TRUE)
 #' 
 #' @export list_reduction
-list_reduction <- function(inlist, fun = intersect, flatten = FALSE, sorted = FALSE) {
-  temp <- Reduce(function(x, y) Map(match.fun(fun), x, y), inlist)
+list_reduction <- function(inlist, FUN = intersect, flatten = FALSE, sorted = FALSE, MoreArgs = NULL) {
+  temp <- Reduce(function(x, y) mapply(FUN, x, y, MoreArgs = MoreArgs, SIMPLIFY = FALSE), inlist)
   if (sorted) temp <- lapply(temp, sort)
   if (flatten) {
     if (all(lengths(temp) == 1L)) {
